f46eaad77879a0f2d9bcc6af16db6d13e773ef13
Igor Nepipenko <inepipenko@muellerone.com>
2020-11-01 12:01:38 +0200

    Feature: live search [ref:no-ref]

--- 5-live-search/src/index.ts
+++ 5-live-search/src/index.ts
@@ -1,3 +1,16 @@
 import 'bootstrap';
 import '../../assets/css/style.css';
-import  './styles.css';
+import './styles.css';
+import { fromEvent } from "rxjs";
+import { liveSearch, request } from "./live-search";
+import { ajax } from "rxjs/ajax";
+
+const inputEl = document.querySelector('#search') as HTMLInputElement;
+const container = document.querySelector('.container') as HTMLDivElement;
+
+liveSearch(
+    fromEvent<InputEvent>(inputEl, 'input'),
+    (text) => request(ajax(`http://api.github.com/search/repositories?q=${text}`)))
+    .subscribe((htmlStr: string) => {
+        container.innerHTML = htmlStr;
+    })
---
+++ 5-live-search/src/live-search.ts
@@ -0,0 +1,79 @@
+import { Observable } from "rxjs";
+import {
+    bufferCount,
+    concatAll,
+    debounceTime,
+    distinctUntilChanged,
+    filter,
+    map,
+    pluck, reduce,
+    switchMap
+} from "rxjs/operators";
+import { AjaxResponse } from "rxjs/ajax";
+
+
+// export interface IResult {
+//     name: string;
+//     description: string,
+//     owner: {
+//         avatar_url: string
+//     }
+// }
+
+class ResultItem {
+    public name: string;
+    public description: string;
+    public avatar_url: string;
+
+    constructor({name, description, owner: {avatar_url}}: any) {
+        this.name = name;
+        this.description = description;
+        this.avatar_url = avatar_url;
+    }
+}
+
+
+export function liveSearch(source1$: Observable<InputEvent>, sourceFn: (text: string) => Observable<any>) {
+    return source1$.pipe(
+        debounceTime(300),
+        pluck<InputEvent, string>('target', 'value'),
+        map((text: string) => text.trim()),
+        filter((text) => text.length > 3),
+        distinctUntilChanged(),
+        switchMap(sourceFn)
+    )
+}
+
+export function request(source$: Observable<AjaxResponse>): Observable<any> {
+    return source$
+        .pipe(
+            pluck<AjaxResponse, any[]>('response', 'items'),
+            concatAll(),
+            map((item) => new ResultItem(item)),
+            map(createCard),
+            bufferCount(3),
+            reduce((resultStr: string, htmlStrs: string[]) => {
+                return `${resultStr}${createRow(htmlStrs)}`
+            }, ''),
+            map((htmlStr) => htmlStr.trim().replace(/\s+(<)/g, '<'))
+        )
+}
+
+
+function createCard(item: ResultItem) {
+    return `<div class="col-md-4 col-sm-6">
+       <div class="card">
+           <img  class="card-img-top" src=${item.avatar_url} alt=${item.name} />
+           <div class="card-body">
+              <h5 class="card-title">${item.name}</h5>
+              <p class="card-text">${item.description}</p>
+           </div>
+       </div>
+    </div>
+    `
+}
+
+
+function createRow(htmlStrs: string[]): string {
+    return `<div class="row">${htmlStrs.join(' ')}</div>`
+}
